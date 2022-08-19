0df2f84b94838507744c1f4e6c8e99396b40dc03
alexliho <lihovidov.alex.dev@gmail.com>
2022-08-19 15:46:53 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\PropertyController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.home');
+});
+
+Route::get('/add-property', function () {
+    return view('pages.add_property');
+})->name('add_property');
+
+Route::post('/add-property', [PropertyController::class, 'addProperty'])->middleware(['auth'])->name('store_property');
+
+Route::get('/my-properties', [PropertyController::class, 'getUserProperties'])->middleware(['auth'])->name('my_properties');
+Route::get('/all-properties', [PropertyController::class, 'getAllProperties'])->name('all_properties');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
